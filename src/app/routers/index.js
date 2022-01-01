c71bd4347224652786381ed871aacbd246938c26
Cao Long <66582805+caolong0204@users.noreply.github.com>
2022-01-01 21:36:56 +0700

    update

--- src/app/routers/index.js
+++ src/app/routers/index.js
@@ -5,26 +5,6 @@ function route(app) {
   app.use("/course", course);
   app.use("/music", music);
   app.use("/person", person);
-  app.get(
-    "/middleware",
-    (req, res, next) => {
-      if (req.query.id == 100) {
-        req.result = "mot tram";
-        return next();
-      }
-      if (req.query.id == 200) {
-        return next();
-      }
-      res.status(403).send("Middleware failll!!!");
-    },
-    (req, res, next) => {
-      res.json({
-        result: req.result,
-        message: "success",
-      });
-    }
-  );
-
   app.get("/", (req, res) => {
     res.send("Home Page");
   });
